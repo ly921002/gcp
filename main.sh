55222864bd629dc2075f18b7668a08dd557f1f4b
ly921002 <92194283+ly921002@users.noreply.github.com>
2025-10-12 21:14:49 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,2 @@
-SSL_CERT_FILE=gcp.240713.xyz.crt nohup ./agent -e https://gcp.240713.xyz -t QlVjtgwxsl3ZwwX0CugyH4
+chmod 777 komari-agent
+SSL_CERT_FILE=gcp.240713.xyz.crt nohup ./komari-agent -e https://gcp.240713.xyz -t QlVjtgwxsl3ZwwX0CugyH4
